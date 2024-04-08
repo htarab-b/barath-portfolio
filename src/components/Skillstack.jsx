0768c1bc2eceb401d91f4206c74e37cb33a41582
Barath B <barath@Baraths-MacBook-Air.local>
2024-04-09 00:58:11 +0530

    Fixed alignments

--- src/components/Skillstack.jsx
+++ src/components/Skillstack.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 import "./css/Skillstack.css";
 import SkillstackImage from "../assets/Skillstack-image.png";
 
@@ -11,53 +11,64 @@ import javascriptimage from "../assets/Tech Stack/javascript.svg";
 import reactimage from "../assets/Tech Stack/react.svg";
 import viteimage from "../assets/Tech Stack/vite.svg";
 // Back-End
-import pythonimage from "../assets/Tech Stack/python.svg"
-import djangoimage from "../assets/Tech Stack/django.svg"
-import nodejsimage from "../assets/Tech Stack/nodejs.svg"
-import phpimage from "../assets/Tech Stack/php.svg"
-import javaimage from "../assets/Tech Stack/java.svg"
+import pythonimage from "../assets/Tech Stack/python.svg";
+import djangoimage from "../assets/Tech Stack/django.svg";
+import nodejsimage from "../assets/Tech Stack/nodejs.svg";
+import phpimage from "../assets/Tech Stack/php.svg";
+import javaimage from "../assets/Tech Stack/java.svg";
 // Database
-import mysqlimage from "../assets/Tech Stack/mysql.svg"
-import sqlserverimage from "../assets/Tech Stack/sqlserver.svg"
-import mongodbimage from "../assets/Tech Stack/mongodb.svg"
-import postgresqlimage from "../assets/Tech Stack/postgresql.svg"
+import mysqlimage from "../assets/Tech Stack/mysql.svg";
+import sqlserverimage from "../assets/Tech Stack/sqlserver.svg";
+import mongodbimage from "../assets/Tech Stack/mongodb.svg";
+import postgresqlimage from "../assets/Tech Stack/postgresql.svg";
 
 const Skillstack = () => {
   return (
     <>
-        <Header />
-        <div className="skillstackmain" style={{ 
-                backgroundImage: `url(${SkillstackImage})`,                
-            }}>
-            <h1>My Tech Stack</h1>
-        </div>
-        <div className="skillstackcontent">
-            <p><strong className="highlight">Front-End Development</strong></p>
-            <p>
-                <img src={html5image} alt="" width="15%" />
-                <img src={css3image} alt="" width="15%" />
-                <img src={javascriptimage} alt="" width="12%" />
-                <img src={reactimage} alt="" width="12%" />
-                <img src={viteimage} alt="" width="12%" />
-            </p>
-            <p><strong className="highlight">Back-End Development</strong></p>
-            <p>
-                <img src={pythonimage} alt="" width="12%" />
-                <img src={djangoimage} alt="" width="12%" />
-                <img src={nodejsimage} alt="" width="12%" />
-                <img src={phpimage} alt="" width="12%" />
-                <img src={javaimage} alt="" width="12%" />
-            </p>
-            <p><strong className="highlight">Database Systems</strong></p>
-            <p>
-                <img src={mysqlimage} alt="" width="15%" />
-                <img src={sqlserverimage} alt="" width="15%" />
-                <img src={mongodbimage} alt="" width="15%" />
-                <img src={postgresqlimage} alt="" width="18%" />
-            </p>
-        </div>
+      <Header />
+      <div
+        className="skillstackmain"
+        style={{
+          backgroundImage: `url(${SkillstackImage})`,
+        }}
+      >
+        <h1>My Tech Stack</h1>
+      </div>
+      <div className="skillstackcontent">
+        <p>
+          <strong className="highlight">Front-End Development</strong>
+        </p>
+        <p>
+          <img src={html5image} alt="" width="15%" />
+          <img src={css3image} alt="" width="15%" />
+          <img src={javascriptimage} alt="" width="12%" />
+          <img src={reactimage} alt="" width="12%" />
+          <img src={viteimage} alt="" width="12%" />
+        </p>{" "}
+        <br />
+        <p>
+          <strong className="highlight">Back-End Development</strong>
+        </p>
+        <p>
+          <img src={pythonimage} alt="" width="12%" />
+          <img src={djangoimage} alt="" width="12%" />
+          <img src={nodejsimage} alt="" width="12%" />
+          <img src={phpimage} alt="" width="12%" />
+          <img src={javaimage} alt="" width="12%" />
+        </p>{" "}
+        <br />
+        <p>
+          <strong className="highlight">Database Systems</strong>
+        </p>
+        <p>
+          <img src={mysqlimage} alt="" width="15%" />
+          <img src={sqlserverimage} alt="" width="15%" />
+          <img src={mongodbimage} alt="" width="15%" />
+          <img src={postgresqlimage} alt="" width="18%" />
+        </p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Skillstack
+export default Skillstack;
