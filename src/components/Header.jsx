afee7624e1e6cc824fe50ee0f90f4dcdd941e96b
Barath B <barath@Baraths-MacBook-Air.local>
2024-04-09 00:38:36 +0530

    Updated contact links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,22 @@ import { NavLink } from "react-router-dom";
 import "./css/Header.css";
 
 function Header() {
-    return (
-        <div className="navbar">
-            <NavLink exact to="/" activeClassName="active">Home</NavLink>
-            <NavLink to="/about" activeClassName="active">About</NavLink>
-            <NavLink to="/skillstack" activeClassName="active">Skill Stack</NavLink>
-            <NavLink to="/contact" className="contact" activeClassName="active">Contact</NavLink>
-        </div>
-    );
+  return (
+    <div className="navbar">
+      <NavLink exact to="/" activeClassName="active">
+        Home
+      </NavLink>
+      <NavLink to="/about" activeClassName="active">
+        About
+      </NavLink>
+      <NavLink to="/skillstack" activeClassName="active">
+        Skill Stack
+      </NavLink>
+      <NavLink to="/contact" className="contact" activeClassName="active">
+        Contact
+      </NavLink>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,30 +2,35 @@ import React from "react";
 import Header from "./Header";
 import Typewriter from "typewriter-effect";
 import "./css/Home.css";
-import HomeImage from "../assets/Home-image.png"
-
+import HomeImage from "../assets/Home-image.png";
 
 function Home() {
-    return(
+  return (
     <>
-        <Header />
-        <div className="homecontent" style={{ 
-                backgroundImage: `url(${HomeImage})`,                
-            }}>
-            <h1>Hi, I'm <strong className="name">Barath</strong><br/>I am a
-            <strong className="highlight">
-                <Typewriter
-                    options={{
-                        strings: ["Developer", "Designer", "Freelancer"],
-                        autoStart: true,
-                        loop: true,
-                    }}
-                />
-            </strong>
-            </h1>
-        </div>
+      <Header />
+      <div
+        className="homecontent"
+        style={{
+          backgroundImage: `url(${HomeImage})`,
+        }}
+      >
+        <h1>
+          Hi, I'm <strong className="name">Barath</strong>
+          <br />I am a
+          <strong className="highlight">
+            <Typewriter
+              options={{
+                strings: ["Developer", "Designer", "Freelancer"],
+                autoStart: true,
+                loop: true,
+              }}
+              className="typewriter"
+            />
+          </strong>
+        </h1>
+      </div>
     </>
-    )
+  );
 }
 
 export default Home;
