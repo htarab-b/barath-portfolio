398868df2203a437e860f7274dde16d79519ad78
Barath B <barath@Baraths-MacBook-Air.local>
2024-06-27 23:56:14 +0530

    Linked form to Gform

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -148,7 +148,7 @@ function Contact() {
                     whileInView = {{ x:0, opacity:1 }}
                     transition = {{ duration:1, delay:0 }}
                 className="relative z-0 w-full mb-5 group">
-                    <input type="text" name="entry.1621349201" id="Name" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required disabled={submitted} />
+                    <input type="text" name="entry.1621349201" id="Name" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required readOnly={submitted} />
                     <label className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
                 </motion.div>
                 <motion.div 
@@ -156,7 +156,7 @@ function Contact() {
                     whileInView = {{ x:0, opacity:1 }}
                     transition = {{ duration:1, delay:0 }}
                 className="relative z-0 w-full mb-5 group">
-                    <input type="email" name="entry.1661846347" id="Email" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required disabled={submitted} />
+                    <input type="email" name="entry.1661846347" id="Email" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required readOnly={submitted} />
                     <label className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
                 </motion.div>
                 <motion.div 
@@ -164,7 +164,7 @@ function Contact() {
                     whileInView = {{ x:0, opacity:1 }}
                     transition = {{ duration:1, delay:0 }}
                 className="relative z-0 w-full mb-5 group">
-                    <textarea name="entry.592867916" id="Message" rows="4" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required disabled={submitted}></textarea>
+                    <textarea name="entry.592867916" id="Message" rows="4" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required readOnly={submitted}></textarea>
                     <label className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Message</label>
                 </motion.div>
                 <motion.input 
